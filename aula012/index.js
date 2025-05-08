1c6fd2b376998efaff2d700b9c36f30da0aa2d93
JhonatanNDS <jhonatannunesdossantos228@gmail.com>
2025-05-08 13:36:13 -0300

    .

---
+++ aula012/index.js
@@ -0,0 +1,7 @@
+var idade = 18
+if (idade < 18){
+    console.log(`Com ${idade} voce é menor de idade`)
+}
+else{
+    console.log(`Com ${idade} voce é maior de idade`)
+}
---
+++ aula014/modelo/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora sao ${hora}h horas`
+    if (hora >+ 0 && hora < 12){
+        img.src = "imagens/manha.png"
+        document.body.style.background = "#e2cd9f"
+    }
+    else if (hora >= 12 && hora < 17){
+        img.src = "imagens/tarde.png"
+        document.body.style.background = "#b9846f"
+        }
+    else{
+        img.src = "imagens/noite.png"
+        document.body.style.background = "#515154f"
+    }
+}
+
